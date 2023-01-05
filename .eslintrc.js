34813b4283c9fdc2945c1f071c33e616028a7beb
maojun <maojun@xiaohongshu.com>
2023-01-05 17:15:08 +0800

    feat(init): init
    all
    
    BREAKING CHANGE: init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  "overrides": [
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "plugins": [
+    "vue",
+    "@typescript-eslint",
+    "prettier",
+  ],
+  "rules": {
+    "prettier/prettier": "error"
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const ab = 1;
+</script>
+
+<template>
+  <div>low code</div>
+</template>
+
+<style scoped>
+
+</style>
